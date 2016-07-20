1c04afee902669a84cddbc641c81cd14806967e1
ahonan <ajhonan26@gmail.com>
2016-07-20 18:53:53 -0500

    Create site.js

---
+++ site.js
@@ -0,0 +1,22 @@
+/*global $*/
+var main=function(){
+$('#menu-icon').click(function(){
+	if($('.nav').hasClass('open')){
+		$('.nav').removeClass('open').animate({left:"-250px"}, 500);
+		$('body').animate({left:"0px"}, 500);
+	}else{
+		$('.nav').addClass('open').animate({left:"0px"}, 500);
+		$('body').animate({left:"250px"}, 500);
+	}
+});
+$('.dropdown').click(function(){
+	if($('.sub-nav').hasClass('o')){
+		$('.sub-nav').toggle(400).removeClass('o');
+		$('#caret').addClass('caret-down').removeClass('caret-up');
+	}else{
+		$('.sub-nav').toggle(400).addClass('o');
+		$('#caret').addClass('caret-up').removeClass('caret-down');
+	}
+});
+};
+$(document).ready(main);
